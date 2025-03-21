11570bbe08880b5ef6576931e9f8822b59a19183
Priyanka <priyanka.kumari@hyperisland.se>
2025-03-21 20:23:02 +0100

    contacts updated

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -14,17 +14,17 @@ export const Contact = () => {
       <ul className={styles.links}>
         <li className={styles.link}>
             <img src={getImageUrl("contact/emailIcon.png")} alt="email icon" />
-            <a href="mailto:prisri0801@gmail.com">prisri0801@gmail.com</a>
+            <a href="mailto:prisri0801@gmail.com" target="_blank">prisri0801@gmail.com</a>
         </li>
 
         <li className={styles.link}>
             <img src={getImageUrl("contact/linkedinIcon.png")} alt="linkedin icon" />
-            <a href="https://www.linkedin.com/in/priyanka-kumari-995901274//">linkedin.com/in/priyanka-kumari-995901274/</a>
+            <a href="https://www.linkedin.com/in/priyanka-kumari-995901274//" target="_blank">linkedin.com/in/priyanka-kumari-995901274/</a>
         </li>
 
         <li className={styles.link}>
             <img src={getImageUrl("contact/githubIcon.png")} alt="github icon" />
-            <a href="https://github.com/priyanka8637kumari">https://github.com/priyanka8637kumari</a>
+            <a href="https://github.com/priyanka8637kumari" target="_blank">https://github.com/priyanka8637kumari</a>
         </li>
       </ul>
     </footer>
